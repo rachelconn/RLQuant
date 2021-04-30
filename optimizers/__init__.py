b72ecee4a0caf47b5ba37e7ea2a59970e8ed0d2f
Ryan Conn <rconn478@gmail.com>
2021-04-30 00:02:12 -0500

    squash

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,3 +1,4 @@
 from .a2c import A2C, load_a2c_model
+from .a2c_lstm import A2C_LSTM, load_a2c_lstm_model
 
-__all__ = ['A2C', 'load_a2c_model']
+__all__ = ['A2C', 'A2C_LSTM', 'load_a2c_model', 'load_a2c_lstm_model']
