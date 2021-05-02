9152b010cb821bc62256c5cdbe1de3bb7ecfcb82
Ryan Conn <rconn478@gmail.com>
2021-05-02 15:39:01 -0500

    Use normal trading environment

--- utils/generate_tickers.py
+++ utils/generate_tickers.py
@@ -3,7 +3,7 @@ import random
 import os
 import numpy as np
 import pandas as pd
-from gym_environments.trading_environment_single_indicator import TradingEnvironment
+from gym_environments.trading_environment import TradingEnvironment
 import backtrader
 
 def get_tickers_from_csv(filename):
@@ -23,30 +23,6 @@ def generate_training_test_environments(filename, training_set_size=100, test_se
             ticker = all_tickers.pop()
             try:
                 env = TradingEnvironment(ticker)
-                return env
-            except Exception:
-                pass
-
-        # Ran out of candidates for environments
-        raise IndexError('Not enough valid tickers in dataset to create environments')
-
-    training_envs = [create_environment() for _ in range(training_set_size)]
-    test_envs = [create_environment() for _ in range(test_set_size)]
-
-    return training_envs, test_envs
-
-def generate_training_test_environments_s_i(filename, training_set_size=100, test_set_size=20, *_, seed=None, indicator=None):
-    all_tickers = list(get_tickers_from_csv(filename))
-
-    assert test_set_size < len(all_tickers), 'Test set size >= number of tickers'
-
-    random.seed(seed)
-    random.shuffle(all_tickers)
-    def create_environment():
-        while all_tickers:
-            ticker = all_tickers.pop()
-            try:
-                env = TradingEnvironment(ticker,indicator)
                 return env
             except Exception:
                 pass
